2a83944cd7e33037cde2db34fd28eeedddf4c3ce
Anirookie <ani007live@outlook.com>
2024-05-29 12:56:33 +0530

    Gemini-Langchain

---
+++ Mulit-PDF_Langchain_GeminiPro/app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader=PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+    chunks=text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store=FAISS.from_text(text_chunks,embedding=embeddings)
+    vector_store.save_local('faiss_index')
+
+
+def get_conversational_chain():
+    prompt_template="""
+    Answer my question in detail and carefully understand the document 
+
+    """
